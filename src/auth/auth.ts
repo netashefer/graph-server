14065bb4c23dd630ddcb99aea2ecd390adf32455
Yahell <yahellodev@gmail.com>
2022-10-09 22:49:49 +0300

    change to another name from localhost

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -3,6 +3,6 @@ import { auth } from 'express-oauth2-jwt-bearer';
 // Authorization middleware. When used, the Access Token must
 // exist and be verified against the Auth0 JSON Web Key Set.
 export const verifyJwt = auth({
-  audience: 'https://localhost:8000',
+  audience: 'https://local.graphit.com:8000',
   issuerBaseURL: `https://graphit.us.auth0.com/`,
 });
